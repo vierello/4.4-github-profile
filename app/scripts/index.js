065e79871b3b3692fe5520d8c344c6353a97b763
vierello <vierello@hotmail.com>
2016-06-09 16:00:01 -0400

    update package.json to include fonts

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var _ = require('underscore');
+var handlebars = require('handlebars');
+var githubtoken = require('./githubapikey.js');
+
+console.log(githubtoken.token);
+
+if(githubtoken !== undefined){
+console.log('testing');
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + githubtoken.token
+    }
+  });
+}
+
+
+
+var myUrl = "https://api.github.com/users/vierello";
+
+$.ajax(myUrl).done(function(data){
+  console.log(data);
+})
